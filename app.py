c630091fa32ccd98ffbe8830fc5d121e3743ac37
Steven Pollack <steven@gnobel.com>
2016-03-16 15:50:12 +0100

    moved caching to redis
    - changed / to not redirect to apiary
    - call locache for expiration time calculations.

--- app.py
+++ app.py
@@ -1,42 +1,38 @@
+import os
+import re
+import redis
+import requests
+import warnings
 from flask import Flask, request, json, Response, redirect
-from requests import HTTPError
-from google_movies_scraper import extract_all_theatres_and_showtimes
-from re import sub
-from warnings import warn
-from iron_cache import IronCache
-from localization_functions import calculate_timeleft_in_day, determine_utc_offset
 
-# use iron_cache for quick and dirty caching...
-# rethinkdb might be smarter, but iron_cache is integrated
-# into heroku...
-#
+from google_movies_scraper import extract_all_theatres_and_showtimes
+from localization_functions import calculate_timeleft_in_day
+
 # Setup 3 caches, one for
 # 1. results
 # 2. query statistics
 # 3. location utc_offsets
 #
 # 1 & 2 should have expiring items
-query_results = IronCache(name='query_results')
-query_counter = IronCache(name='query_counter')
-utc_offsets = IronCache(name='utc_offsets')
+query_results = redis.from_url(os.environ.get('REDIS_URL'), 0)
+query_counter = redis.from_url(os.environ.get('REDIS_URL'), 1)
+utc_offsets = redis.from_url(os.environ.get('REDIS_URL'), 2)
 
 def calculate_expiration_time(near):
-    # use a 'normalized' version of `near` as the cache_key;
-    # the other caches append the `days_from_now` parameter to this
-    # value as the cache_key, but our timezone cache shouldn't care
-    # about this parameter
-    cache_key = sub('[ ,]*', '', near).lower()
-    try:
-        utc_offset = int(utc_offsets.get(key=cache_key).value)
-    except HTTPError:
-        utc_offset = determine_utc_offset(near)
-        utc_offsets.put(key=cache_key, value=str(utc_offset))
+    locache_url = 'https://locache.herokuapp.com/'
 
-    return(calculate_timeleft_in_day(utc_offset))
+    r = requests.get(locache_url, params={'location': near})
+    utc_offset = r.json().get('utcOffset')
 
-def create_cache_key(near, days_from_now=None, separator='-'):
+    if utc_offset is None:
+        warnings.warn('%s?location=%s failed to determine UTC Offset' % (locache_url, near))
+
+    return calculate_timeleft_in_day(utc_offset)
+
+
+def create_cache_key(near, days_from_now=None, sep=':'):
     # The cache_key pattern is:
-    #     near_normalize + separator + days_from_now
+    #     near_normalize + sep + days_from_now
     # However: google ignores negative dates, which means that the
     # results for days_from_now = -1 will be the same as
     # days_from_now = -7, which means there's no reason to cache
@@ -44,22 +40,23 @@ def create_cache_key(near, days_from_now=None, separator='-'):
     if (days_from_now is not None and int(days_from_now) < 0) or (days_from_now is None):
         days_from_now = '0'
 
-    near_normalized = sub('[ ,]*', '', near).lower()
+    weird_tokens = '!@#$%^&*()_=+,<.>/?;:\'"[]{}|\\'
+    regexp = '[ ' + weird_tokens + ']*'
 
-    return near_normalized + separator + days_from_now
+    near_normalized = re.sub(regexp, '', near).lower()
+
+    return '%s%s%s' % (near_normalized, sep, days_from_now)
 
 def get_showtimes_from_google_and_cache(near, days_from_now, cache_key):
     try:
         showtimes = json.dumps(extract_all_theatres_and_showtimes(near, days_from_now))
         status = 200
         # populate caches
-        cache_options = {
-            'expires_in': calculate_expiration_time(near)
-        }
-        query_results.put(key=cache_key, value=showtimes, options=cache_options)
-        query_counter.put(key=cache_key, value=1, options=cache_options)
+        cache_ex = calculate_expiration_time(near)
+        query_results.set(name=cache_key, value=showtimes, ex=cache_ex)
+        query_counter.set(name=cache_key, value=1, ex=cache_ex)
     except Exception as e:
-        warn(str(e))
+        warnings.warn(str(e))
         showtimes = json.dumps({'error': str(e)})
         status = 500  # internal server error
 
@@ -67,13 +64,37 @@ def get_showtimes_from_google_and_cache(near, days_from_now, cache_key):
 
 def get_showtimes_from_cache(cache_key):
     # iron_cache stores keys as long strings...
-    showtimes = query_results.get(key=cache_key).value
-    query_counter.increment(key=cache_key)
-    status = 200
-    return showtimes, status
+    showtimes = query_results.get(name=cache_key)
+    if showtimes is not None:
+        query_counter.incr(name=cache_key)
+        status = 200
+        return showtimes, status
+    else:
+        return None, None
 
 app = Flask(__name__)
 @app.route('/')
+def home():
+    return """
+    <p>main-endpoint:
+     <a href='/movies'>
+        google.com-movies-scraper.herokuapp.com/movies?{near[,date]}
+     </a>
+    </p>
+    <p>docs (apiary.io):
+     <a href='/docs'>
+        google-movies-scraper.herokuapp.com/docs
+     </a>
+    </p>
+    <p>github:
+     <a href='https://github.com/stevenpollack/gms'>
+        https://github.com/stevenpollack/gms
+     </a>
+    </p>
+    """
+
+
+@app.route('/docs')
 def route_to_apiary():
     apiary_io = 'http://docs.googlemoviesscraper.apiary.io/'
     return (redirect(apiary_io, code=302))
@@ -91,18 +112,20 @@ def get_showtimes():
     else:
         try:
             cache_key = create_cache_key(near, days_from_now)
-            showtimes, status = get_showtimes_from_cache(cache_key)
-            warn('Fetched results from cache with key: ' + cache_key)
         except ValueError as ve:
-            warn(str(ve))
-            showtimes = json.dumps({'error': '`date` must be a base-10 integer'})
-            status = 400
-        except HTTPError as e:
-            warn(str(e))
+            warnings.warn(str(ve))
+            return Response(json.dumps({'error': '`date` must be a base-10 integer'}),
+                            status=400,
+                            mimetype=mimetype)
+
+        showtimes, status = get_showtimes_from_cache(cache_key)
+
+        if showtimes is not None:
+            warnings.warn('Fetched results from cache with name: ' + cache_key)
+        else:
             showtimes, status = get_showtimes_from_google_and_cache(near, days_from_now, cache_key)
 
-    resp = Response(showtimes, status=status, mimetype=mimetype)
-    return (resp)
+    return Response(showtimes, status=status, mimetype=mimetype)
 
 if (__name__ == '__main__'):
     app.run(debug=False, host='0.0.0.0', port=5000)
